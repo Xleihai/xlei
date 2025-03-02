6b18e408d413997e937ca3deceda677c2a8e9cb8
Xleihai <a172401236@qq.com>
2025-03-02 15:59:42 +0800

    v0.2.0 初版

--- README.md
+++ README.md
@@ -1,36 +1,27 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+# 星泪不流泪
 
-## Getting Started
+我的个人主页，用于 react next 学习
 
-First, run the development server:
+## 快速开始
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+### 安装依赖
+
+```sh
+pnpm i
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+### 运行开发环境
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+```sh
+pnpm dev
+```
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+### 构建生产环境
 
-## Learn More
+```sh
+pnpm build
+```
 
-To learn more about Next.js, take a look at the following resources:
+## 许可证
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+[MIT](LICENSE)
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,79 @@
-import styles from "./page.module.css";
+"use client";
+import { useState } from "react";
+import styles from "./page.module.scss";
 
 export default function Home() {
+  const [sayings, setSayings] = useState({
+    author: "毛泽东",
+    content: "不要陷于狭隘的经验论。"
+  });
+  const linkList = [
+    {
+      name: "首页",
+      url: "https://isxlei.fun/"
+    },
+    {
+      name: "博客",
+      url: "https://blog.isxlei.fun/"
+    },
+    {
+      name: "监控",
+      url: "https://status.isxlei.fun/"
+    }
+  ];
+  const iconLink = [
+    {
+      name: "GitHub",
+      url: "https://github.com/Xleihai",
+      icon: "iconfont icon-github"
+    },
+    {
+      name: "Email",
+      url: "mailto:isxlei@foxmail.com",
+      icon: "iconfont icon-email"
+    }
+  ];
   return (
     <div className={styles.page}>
-      <main>星泪不流泪</main>
+      <div className={styles.masked}></div>
+      <main className={styles.main}>
+        <div className={styles.content}>
+          <div className={styles.content_inner}>
+            <div className={styles.avatar}>
+              <img src="/imger/avatar.jpg" alt="avatar" />
+            </div>
+            <p className={styles.name}>星泪不流泪</p>
+            <p className={styles.title}>沉淀过往，扎根当下，奔赴未来</p>
+            <div className={styles.hr} />
+            <p className={styles.sayings}>{sayings.content}</p>
+            <p className={styles.sayings}>「{sayings.author}」</p>
+            <div className={styles.link_box}>
+              {linkList.map(item => (
+                <a href={item.url} target="_blank" className={styles.link} key={item.name}>
+                  {item.name}
+                </a>
+              ))}
+            </div>
+            <div className={styles.icon_box_link}>
+              {iconLink.map(item => (
+                <a href={item.url} target="_blank" className={styles.icon_link} key={item.name}>
+                  <span className={item.icon}></span>
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
+  const getSayings = async () => {
+    const res = await fetch("https://v1.hitokoto.cn/?c=d");
+    const data = await res.json();
+    console.log(data);
+    setSayings({
+      author: data.from_who,
+      content: data.hitokoto
+    });
+  };
+  // getSayings();
 }
